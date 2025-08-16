69008544046d6ad87219b72d35b151220addd16d
facundosola <solafacundo9@gmail.com>
2025-08-16 17:36:29 -0300

    feat: add e2e test for deposit and balance flow

--- test/Betsson.OnlineWallets.ApiTests/OnlineWalletControllerApiTests.cs
+++ test/Betsson.OnlineWallets.ApiTests/OnlineWalletControllerApiTests.cs
@@ -1,6 +1,5 @@
 using Betsson.OnlineWallets.Web.Models;
 using Microsoft.AspNetCore.Mvc.Testing;
-//using Microsoft.VisualStudio.TestPlatform.TestHost;
 using System.Net.Http.Json;
 using Betsson.OnlineWallets.Web;
 
@@ -15,38 +14,92 @@ namespace Betsson.OnlineWallets.ApiTests
         public OnlineWalletControllerApiTests(WebApplicationFactory<Program> factory)
         {
             _client = factory.CreateClient();
-       
+
         }
 
         [Fact, Trait("GET", "onlineWallet/balance")]
         public async Task GetBalance_ReturnsOkAndZeroBalanceInitially()
         {
-            decimal initialBalance = 0m;
+
             var response = await _client.GetAsync("/onlinewallet/balance");
             response.EnsureSuccessStatusCode();
 
             var balance = await response.Content.ReadFromJsonAsync<BalanceResponse>();
             Assert.NotNull(balance);
-            Assert.Equal(initialBalance, balance.Amount);
+            Assert.True(balance.Amount >= 0);
         }
 
-        [Fact, Trait("POST", "onlineWallet/deposit")]
-        public async Task DepositFunds_WithValidAmount_IncrementsBalanceCorrectly()
+        [Fact, Trait("GET", "onlineWallet/balance")]
+        public async Task GetBalance_AfterDeposit_ShoulBeCorrect()
         {
-
+            //Arrange            
             var initialBalance = await _client.GetFromJsonAsync<BalanceResponse>("/onlinewallet/balance");
             Assert.NotNull(initialBalance);
+            var depositRequest = new DepositRequest { Amount = 1515.75m };
 
-            var depositRequest = new DepositRequest { Amount = 100m };
-            var response = await _client.PostAsJsonAsync("/onlinewallet/deposit", depositRequest);
+            //Act
+            var depositResponse = await _client.PostAsJsonAsync("/onlinewallet/deposit", depositRequest);
+            depositResponse.EnsureSuccessStatusCode();
+            var response = await _client.GetAsync("/onlinewallet/balance");
             response.EnsureSuccessStatusCode();
             var balance = await response.Content.ReadFromJsonAsync<BalanceResponse>();
+
+            //Assert
             Assert.NotNull(balance);
             var expectedBalance = initialBalance.Amount + depositRequest.Amount;
             Assert.Equal(expectedBalance, balance.Amount);
 
         }
-       
 
+        [Fact, Trait("POST", "onlineWallet/deposit")]
+        public async Task DepositFunds_WithValidAmount_ProcessCorrectly()
+        {
+
+            //Arrange
+            var initialBalanceResponse = await _client.GetFromJsonAsync<BalanceResponse>("/onlinewallet/balance");
+            Assert.NotNull(initialBalanceResponse);
+            decimal depositAmount = 15750.75m;
+            var depositRequest = new DepositRequest { Amount = depositAmount };
+            var expectedBalance = initialBalanceResponse.Amount + depositAmount;
+
+            //Act
+            var response = await _client.PostAsJsonAsync("/onlinewallet/deposit", depositRequest);
+            response.EnsureSuccessStatusCode();
+
+            var balance = await response.Content.ReadFromJsonAsync<BalanceResponse>();
+            Assert.NotNull(balance);
+            Assert.Equal(expectedBalance, balance.Amount);
+
+        }
+
+        [Fact, Trait("POST", "onlineWallet/deposit")]
+        public async Task DepositFunds_WithNegativeAmout_ReturnsBadRequest()
+        {
+            //Arrange
+            decimal negativeDepositAmount = -1500.00m;
+            var depositRequest = new DepositRequest { Amount = negativeDepositAmount };
+            //Act
+            var response = await _client.PostAsJsonAsync("/onlinewallet/deposit", depositRequest);
+
+            //Assert
+            Assert.Equal(System.Net.HttpStatusCode.BadRequest, response.StatusCode);
+
+        }
+
+        [Fact, Trait("POST", "onlineWallet/deposit")]
+        public async Task DepositFunds_WithZeroAmount_ProcessCorrectly()
+        {
+            var depositAmount = 0m;
+            var depositRequest = new DepositRequest { Amount = depositAmount };
+
+            var response = await _client.PostAsJsonAsync("/onlinewallet/deposit", depositRequest);
+            response.EnsureSuccessStatusCode();
+
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+
+        }
+
+   
     }
+    
 }
